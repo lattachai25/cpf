568180c0a114331bae1d88d7de9f3c756f730be0
nextcis <42366333+lattachai25@users.noreply.github.com>
2021-08-01 23:10:08 +0700

    Update create.blade.php

--- resources/views/admin/Poultry/create.blade.php
+++ resources/views/admin/Poultry/create.blade.php
@@ -62,7 +62,7 @@
               <label for="exampleFormControlTextarea1"><b>Brand</b></label>
               <select class="form-control" name="brand">
               @foreach($brand as $brands)
-                <option>{{ $brands->name_brand_en }}</option>
+                <option value="{{ $brands->name_brand_en }}">{{ $brands->name_brand_en }}</option>
               @endforeach  
               </select>
             </div>
@@ -70,7 +70,7 @@
               <label for="exampleFormControlTextarea1"><b>Category</b></label>
               <select class="form-control" name="category">
               @foreach($category as $categorys)
-                <option>{{ $categorys->name_categories }}</option>
+                <option value="{{ $categorys->name_categories }}">{{ $categorys->name_categories }}</option>
               @endforeach  
               </select>
             </div>
@@ -79,7 +79,7 @@
               <label for="exampleFormControlTextarea1"><b>SubCategory</b></label>
               <select class="form-control" name="sub_category">
               @foreach($subcategory as $subcategorys)
-                <option>{{ $subcategorys->name_sub_categories }}</option>
+                <option value="{{ $subcategorys->name_sub_categories }}">{{ $subcategorys->name_sub_categories }}</option>
               @endforeach  
               </select>
             </div>
@@ -107,11 +107,11 @@
             </div>
             <div class="form-group">
               <label for="exampleFormControlInput1"><b>Detel Product EN</b></label>
-              <input type="test" class="form-control" name="detel_product_en">
+              <textarea class="ckeditor form-control" name="detel_product_en"></textarea>
             </div>
             <div class="form-group">
               <label for="exampleFormControlInput1"><b>Detel Product TH</b></label>
-              <input type="test" class="form-control" name="detel_product_th">
+              <textarea class="ckeditor form-control" name="detel_product_th"></textarea>
             </div>
 
 
@@ -121,30 +121,37 @@
 
 
             <div class="form-group">
+              <label for="exampleFormControlTextarea1"><b>images1</b></label>
               <label for="exampleFormControlFile1"> <br> File input MaxSize 400px - 600px / Size less than 1M </b></label>
               <input type="file" name="images_product1" class="form-control-file">
             </div>
             <div class="form-group">
+            <label for="exampleFormControlTextarea1"><b>images2</b></label>
               <label for="exampleFormControlFile1"> <br> File input MaxSize 400px - 600px / Size less than 1M </b></label>
               <input type="file" name="images_product2" class="form-control-file">
             </div>
             <div class="form-group">
+            <label for="exampleFormControlTextarea1"><b>images3</b></label>
               <label for="exampleFormControlFile1"> <br> File input MaxSize 400px - 600px / Size less than 1M </b></label>
               <input type="file" name="images_product3" class="form-control-file">
             </div>
             <div class="form-group">
+            <label for="exampleFormControlTextarea1"><b>images4</b></label>
               <label for="exampleFormControlFile1"> <br> File input MaxSize 400px - 600px / Size less than 1M </b></label>
               <input type="file" name="images_product4" class="form-control-file">
             </div>
             <div class="form-group">
+            <label for="exampleFormControlTextarea1"><b>images5</b></label>
               <label for="exampleFormControlFile1"> <br> File input MaxSize 400px - 600px / Size less than 1M </b></label>
               <input type="file" name="images_product5" class="form-control-file">
             </div>
             <div class="form-group">
+            <label for="exampleFormControlTextarea1"><b>images5</b></label>
               <label for="exampleFormControlFile1"> <br> File input MaxSize 400px - 600px / Size less than 1M </b></label>
               <input type="file" name="images_product6" class="form-control-file">
             </div>
             <div class="form-group">
+            <label for="exampleFormControlTextarea1"><b>Attachment </b></label>
               <label for="exampleFormControlFile1"> <br> File input  Attachment / MaxSize 1M </b></label>
               <input type="file" name="attachment" class="form-control-file">
             </div>
